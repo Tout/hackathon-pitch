0b5d83928f9925776d2a9d5ffd53cc6f64db6631
Jeremia Kimelman <jbkimelman@gmail.com>
2012-11-21 15:18:26 -0800

    new code for camera handling

---
+++ deck.js
@@ -0,0 +1,90 @@
+var Deck = (function() {
+    var latestTout = {'uid': null, 'created_at': new Date(0), 'mp4_url': null}, callAjax, getLatestTout, recordTout;
+
+    callAjax = function(api_call) {
+        var ajax_url = 'https://api.tout.com/api/v1/'+api_call+'?access_token=661c8850e011ac97f83889ba5c516eadcecb0e8da6461d14e748afd295a396d9';
+        
+        return $.ajax({
+            url: ajax_url,
+            async: true
+        });
+    };
+
+    getLatestTout = function() {
+        var x;
+        var whenToutsLoaded = callAjax('users/jeremiak/touts');
+        
+        whenToutsLoaded.done(function (data) {
+            var touts = data['touts'], t;
+
+            if (typeof data['touts'] === 'undefined') {
+                try {
+                    touts = JSON.parse(data)['touts'];
+                } catch (err) { console.log(err); }
+            }
+
+            t = touts[0]['tout'];
+
+            latest = {};
+            latest['uid'] = t['uid'];
+            latest['created_at'] = new Date(t['created_at']);
+            latest['mp4_url'] = t['video']['mp4']['http_url'];
+
+            if (latest['created_at'] > this.latestTout['created_at']){
+                var p = _V_('fresh-tout-video');
+                p.src(latest['mp4_url']);
+
+                this.latestTout = latest;
+            }
+
+        }.bind(this));
+
+    };
+    
+    recordTout = function() {
+        setTimeout(function() {
+            this.getLatestTout();
+            $('#record-button').css('opacity', .6);
+        }.bind(this), 60000);
+        window.open('http://www.tout.com/m/new?display=popup', 'recordTout', 'height=420,width=800');
+    };
+
+    checkLatest = function(tout) {
+        if (tout != 'fool') {
+            var whenToutsLoaded = callAjax('touts/'+tout);
+            whenToutsLoaded.done(function (data) {
+                var tout = data['tout'];
+
+                if (typeof data['tout'] === 'undefined') {
+                    try {
+                        touts = JSON.parse(data)['tout'];
+                    } catch (err) { console.log(err); }
+                }
+
+                console.log(tout);
+
+                this.latestTout = tout['video']['mp4']['http_url'];
+                var p = _V_('fresh-tout-video');
+                p.src(this.latestTout);
+                $('.vjs-controls').css('display', 'none');
+                
+            }.bind(this));               
+
+        }
+    };
+
+    return {
+        latestTout: latestTout,
+        callAjax: callAjax,
+        checkLatest: checkLatest,
+        getLatestTout: getLatestTout,
+        recordTout: recordTout
+    }
+})();
+
+$(document).ready(function(){
+    console.log('ready');
+    $('.vjs-controls').css('display', 'none');
+    Deck.getLatestTout();
+
+}); 
